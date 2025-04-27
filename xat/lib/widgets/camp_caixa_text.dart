0d08c490d56d51c65a4a1fac51a5dd795265f7a6
Yago Yàsser <yagoyasser2001@gmail.com>
2025-04-28 01:01:27 +0200

    Implementació comportament caixa de text Enviar missatge

--- xat/lib/widgets/camp_caixa_text.dart
+++ xat/lib/widgets/camp_caixa_text.dart
@@ -5,6 +5,9 @@ class CampCaixaText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controladorText = TextEditingController();
+    final focusNode = FocusNode();
+
     final VoraCaixaEnviarMissatge = UnderlineInputBorder(
       borderSide: BorderSide(
         color: Colors.transparent),
@@ -12,24 +15,31 @@ class CampCaixaText extends StatelessWidget {
     );
 
     final CaixaEnviarMissatge = InputDecoration(
+      hintText: 'Missatge',
       enabledBorder: VoraCaixaEnviarMissatge,
         focusedBorder: VoraCaixaEnviarMissatge,
         filled: true,
         suffixIcon: IconButton(
           icon: Icon(Icons.send_outlined),
           onPressed: () {
-            // Fer coses
+            final missatge = controladorText.value.text;
+            print('Missatge enviat: $missatge');
+            controladorText.clear();
           }
         ),
     );
 
     return TextFormField(
+      controller: controladorText,
       decoration: CaixaEnviarMissatge,
+      focusNode: focusNode,
       onFieldSubmitted: (value) {
         print('Tecla Enter premuda. Missatge: $value');
+        controladorText.clear();
+        focusNode.requestFocus(); // El teclat no es minimitzarà una vegada premut Enter/Fet
       },
-      onChanged: (value) {
-        print('Missatge actualitzat: $value');
+      onTapOutside: (event) {
+        focusNode.unfocus(); // El teclat es minimitzarà si es prem fora del teclat
       },
     );
   }
